36a8a2401c862fbdb2a7952a8c108e902d2fda7d
Sanan Ali <sananali340@gmail.com>
2024-06-06 22:23:10 +0500

    good

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,26 +1,20 @@
-// store.js
-
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './userRelated/userSlice';
 import { studentReducer } from './studentRelated/studentSlice';
 import { noticeReducer } from './noticeRelated/noticeSlice';
 import { sclassReducer } from './sclassRelated/sclassSlice';
 import { teacherReducer } from './teacherRelated/teacherSlice';
 import { complainReducer } from './complainRelated/complainSlice';
-import { assignmentsReducer } from './assignmentRelated/assignmentsReducers'; // Make sure to export assignmentsReducer
-
-const rootReducer = combineReducers({
-    user: userReducer,
-    student: studentReducer,
-    teacher: teacherReducer,
-    notice: noticeReducer,
-    complain: complainReducer,
-    sclass: sclassReducer,
-    assignments: assignmentsReducer, // Add assignments reducer here
-});
 
 const store = configureStore({
-    reducer: rootReducer,
+    reducer: {
+        user: userReducer,
+        student: studentReducer,
+        teacher: teacherReducer,
+        notice: noticeReducer,
+        complain: complainReducer,
+        sclass: sclassReducer
+    },
 });
 
 export default store;
