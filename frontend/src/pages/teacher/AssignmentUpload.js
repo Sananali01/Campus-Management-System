89cc67a2fad554f9b270c05d46cfd3e87e5d9488
Sanan Ali <sananali340@gmail.com>
2024-06-12 14:57:24 +0500

    update

--- frontend/src/pages/teacher/AssignmentUpload.js
+++ frontend/src/pages/teacher/AssignmentUpload.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useNavigate} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+
 import { Box, Button, Typography, Snackbar, Alert, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import { Delete, InsertDriveFile, CloudDownload, CheckCircle } from '@mui/icons-material'; // Import icons
 import axios from 'axios';
