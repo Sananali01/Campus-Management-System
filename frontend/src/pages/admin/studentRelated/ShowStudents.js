2caa808168665a4c6bd1ab014c2a614f4b547fd9
Sanan Ali <sananali340@gmail.com>
2024-06-13 18:56:40 +0500

    update

--- frontend/src/pages/admin/studentRelated/ShowStudents.js
+++ frontend/src/pages/admin/studentRelated/ShowStudents.js
@@ -43,10 +43,10 @@ const ShowStudents = () => {
     const [message, setMessage] = React.useState("");
 
    const deleteHandler = (deleteID, address) => {
-        console.log(deleteID);
-        console.log(address);
-        // setMessage("Sorry the delete function has been disabled for now.")
-        setShowPopup(true)
+        // console.log(deleteID);
+        // console.log(address);
+        // // setMessage("Sorry the delete function has been disabled for now.")
+        // setShowPopup(true)
 
         dispatch(deleteUser(deleteID, address)).then(() => {
             dispatch(getAllStudents(currentUser._id));
